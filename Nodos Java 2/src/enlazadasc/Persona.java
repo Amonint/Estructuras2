13a1fbfc3a44d5eeefcf3b0e485c254fc54ba355
Amonint <payala909@gmail.com>
2022-07-06 08:46:03 -0500

    ..

---
+++ Nodos Java 2/src/enlazadasc/Persona.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enlazadasc;
+
+public class Persona {
+    int id, edad;
+    String nombre;
+    int[] notas;
+    public String tipo;
+
+    public Persona(int _id, String _nombre, int _edad, int n1, int n2, int n3) {
+        this.id = _id;
+        this.nombre = _nombre;
+        this.edad = _edad;
+        this.notas = new int[3];
+        this.notas[0] = n1;
+        this.notas[1] = n2;
+        this.notas[2] = n3;
+    }
+
+}
