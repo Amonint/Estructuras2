960a36048a4bc70f8bfba2ff45774bea22230063
Amonint <payala909@gmail.com>
2022-07-05 23:42:24 -0500

    Lista doble

---
+++ Listas Doblemente Enlazadas/src/Ejecutor.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Ejecutor {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        ListaDoble lD = new ListaDoble();
+
+        int opc;
+        do {
+
+            System.out.println("Ingresar elementos              [1]:");
+            System.out.println("Eliminar elemento               [2]:");
+            System.out.println("Intercambiar nodos              [3]:");
+            System.out.println("Presentar                       [4]:");
+            System.out.println("Salir                           [0]:");
+            opc = entrada.nextInt();
+
+            if (opc == 1) {
+                boolean bandera = true;
+                while (bandera) {
+                    int i = 1, num;
+                    System.out.println("Ingrese un número a insertar");
+                    num = entrada.nextInt();
+                    lD.insertar(i, num);
+                    i++;
+                    entrada.nextLine();
+                    System.out.println("Ingrese s para dejar de ingresar números");
+                    String a = entrada.nextLine();
+                    if ("s".equals(a) || "S".equals(a)) {
+                        bandera = false;
+                    }
+                }
+            } else {
+                if (opc == 2) {
+
+                    int num;
+                    System.out.println("Ingrese la posición a eliminar");
+                    num = entrada.nextInt();
+                    lD.borrar(num);
+                } else {
+                    if (opc == 3) {
+                        int num, num2;
+                        System.out.println("Ingrese la posición 1 a intercambiar:");
+                        num = entrada.nextInt();
+                        System.out.println("Ingrese la posición 2 a intercambiar:");
+                        num2 = entrada.nextInt();
+                        lD.intercambiar(num, num2);
+
+                    } else {
+                        if (opc == 4) {
+                            lD.imprimir();
+                        }
+                    }
+                }
+            }
+
+        } while (opc != 0);
+
+    }
+
+}
+}
---
+++ Listas Doblemente Enlazadas/src/ListaDoble.java
@@ -0,0 +1,156 @@
+import java.util.Scanner;
+
+public class ListaDoble {
+    public class listaDoble {
+
+        public class Nodo {
+
+            int tail;
+            Nodo anterior, siguiente;
+        }
+
+        private Nodo head;
+
+        public listaDoble() {
+            head = null;
+        }
+
+        void insertar(int pos, int x) {
+            if (pos <= cantidad() + 1) {
+                Nodo nuevo = new Nodo();
+                nuevo.tail = x;
+                if (pos == 1) {
+                    nuevo.siguiente = head;
+                    if (head != null) {
+                        head.anterior = nuevo;
+                    }
+                    head = nuevo;
+                } else if (pos == cantidad() + 1) {
+                    Nodo actual = head;
+                    while (actual.siguiente != null) {
+                        actual = actual.siguiente;
+                    }
+                    actual.siguiente = nuevo;
+                    nuevo.anterior = actual;
+                    nuevo.siguiente = null;
+                } else {
+                    Nodo act = head;
+                    for (int i = 1; i <= pos - 2; i++) {
+                        act = act.siguiente;
+                    }
+                    Nodo sig = act.siguiente;
+                    act.siguiente = nuevo;
+                    nuevo.anterior = act;
+                    nuevo.siguiente = sig;
+                    sig.anterior = nuevo;
+                }
+            }
+        }
+
+        public int extraer(int pos) {
+            if (pos <= cantidad()) {
+                int informacion;
+                if (pos == 1) {
+                    informacion = head.tail;
+                    head = head.siguiente;
+                    if (head != null) {
+                        head.anterior = null;
+                    }
+                } else {
+                    Nodo actual;
+                    actual = head;
+                    for (int f = 1; f <= pos - 2; f++) {
+                        actual = actual.siguiente;
+                    }
+                    Nodo prox = actual.siguiente;
+                    actual.siguiente = prox.siguiente;
+                    Nodo siguiente = prox.siguiente;
+                    if (siguiente != null) {
+                        siguiente.anterior = actual;
+                    }
+                    informacion = prox.tail;
+                }
+                return informacion;
+            } else {
+                return Integer.MAX_VALUE;
+            }
+        }
+
+        public void borrar(int pos) {
+            if (pos <= cantidad()) {
+                if (pos == 1) {
+                    head = head.siguiente;
+                    if (head != null) {
+                        head.anterior = null;
+                    }
+                } else {
+                    Nodo reco;
+                    reco = head;
+                    for (int f = 1; f <= pos - 2; f++) {
+                        reco = reco.siguiente;
+                    }
+                    Nodo prox = reco.siguiente;
+                    prox = prox.siguiente;
+                    reco.siguiente = prox;
+                    if (prox != null) {
+                        prox.anterior = reco;
+                    }
+                }
+            }
+        }
+
+        public void intercambiar(int pos1, int pos2) {
+            if (pos1 <= cantidad() && pos2 <= cantidad()) {
+                Nodo reco1 = head;
+                for (int f = 1; f < pos1; f++) {
+                    reco1 = reco1.siguiente;
+                }
+                Nodo reco2 = head;
+                for (int f = 1; f < pos2; f++) {
+                    reco2 = reco2.siguiente;
+                }
+                int aux = reco1.tail;
+                reco1.tail = reco2.tail;
+                reco2.tail = aux;
+            }
+        }
+
+        public int cantidad() {
+            int cant = 0;
+            Nodo reco = head;
+            while (reco != null) {
+                reco = reco.siguiente;
+                cant++;
+            }
+            return cant;
+        }
+
+        public boolean existe(int n) {
+            Nodo actual = head;
+            while (actual != null) {
+                if (actual.tail == n) {
+                    return true;
+                }
+                actual = actual.siguiente;
+            }
+            return false;
+        }
+
+        public boolean listavacia() {
+            return head == null;
+        }
+
+        public void imprimir() {
+            Nodo reco = head;
+            while (reco != null) {
+                System.out.print(reco.tail + "\t");
+                reco = reco.siguiente;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public void insertar(int i, int num) {
+    }
+}
