aae1dcb82454d3a15859b891c5b554e533e24e5e
AMONINT <payala909@gmail.com>
2022-07-18 11:41:30 -0500

    .....

--- AVLTRE/AVLTree.java
+++ AVLTRE/AVLTree.java
@@ -5,11 +5,18 @@ public class MetodosAVL {
         opc=myArbol.menu();
         switch (opc) {
             case 1:
-                
+                System.out.println("diguiotaadas");
+                num= myArbol.entrada.nextInt();
+                myArbol.raiz= myArbol.insertaEquilibrado(myArbol.raiz,num);
+                break;
+            case 2:
+                myArbol.preOrden(myArbol.raiz);
+                System.out.println("\n");
                 break;
         
-            default:
                 break;
+                
         }
     }
+    
 }
--- AVLTRE/MetodosAvlT.java
+++ AVLTRE/MetodosAvlT.java
@@ -10,6 +10,72 @@ public class MetodosAvlT {
         this.raiz = null;
         this.altura = false;
     }
+    public NodoAvl rotaID(NodoAvl n, nodoAvl n1) {
+        NodoAvl n2 = n1.der;
+        n.izq= n2.der;
+        n2.der=n;
+        n1.der = n2.izq;
+        n2.izq=n1;
+        if (n2.fe == 1) {
+            n1.fe=-1;
+        }else{
+            n1.fe=0;
+
+        }if (n2.fe==-1) {
+            n2.fe =1;
+            
+
+        }else{
+            n.fe =0;
+            n2.fe=0;
+            return n2;
+        }
+    }
+    public NodoAvl rotaDI(NodoAvl n, nodoAvl n1) {
+        NodoAvl n2 = n1.izq;
+        n.der= n2.izq;
+        n2.izq=n;
+        n1.izq = n2.der;
+        n2.der=n1;
+        if (n2.fe == 1) {
+            n1.fe=-1;
+        }else{
+            n1.fe=0;
+
+        }if (n2.fe==-1) {
+            n2.fe =1;
+            
+
+        }else{
+            n.fe =0;
+            n2.fe=0;
+            return n2;
+        }
+    }
+    public NodoAvl rotacionesII(NodoAvl n, nodoAvl n1) {
+        n.izq = n1.der;
+        n1.der=n;
+        if (n1.fe == -1) {
+            n.fe=0;
+            n1.fe =0;
+        }else{
+            n.fe=-1;
+            n1.fe=-1;
+        }
+        return n1;
+    }
+    public NodoAvl rotacionesDD(NodoAvl n, nodoAvl n1) {
+        n.der = n1.izq;
+        n1.izq=n;
+        if (n1.fe == n1) {
+            n.fe=0;
+            n1.fe =0;
+        }else{
+            n.fe=1;
+            n1.fe=-1;
+        }
+        return n1;
+    }
 
     public void insertaEquilibrado(NodoAvl actual, int num) {
         if (actual == null) {
@@ -21,22 +87,21 @@ public class MetodosAvlT {
             actual.der = insertaEquilibrado(actual.der, num);
             if (altura) {
                 switch (actual.fe) {
-                    case -1:
+                    case 1:
                         actual.fe = 0;
                         altura = false;
                         break;
-
-                    default:
                     case 0:
-                        actual.fe = 1;
+                        actual.fe = -1;
                         break;
 
-                    case 1:
+                    case -1:
                         NodoAvl n1= actual.der;
-                        if (n1.fe == 1) {
-                            actual = rotacionDD(actual,n1);
+                        if (n1.fe == -1) {
+                            actual = rotacionII(actual,n1);
                         }else{
-                            actual=rotacionesDI(actual,n1);
+                            actual=rotacionesID(actual,n1);
+                            altura= false;
                         }
                         
                         break;
